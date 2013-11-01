5697ca2891d4d317005ac8f35db37afe603014a3
Tyler Roussos <tylerroussos@gmail.com>
2013-11-01 13:53:04 -0400

    added unit test for the codableValue

--- src/elevate/HVObjects/GenericTypes/CodableValue.php
+++ src/elevate/HVObjects/GenericTypes/CodableValue.php
@@ -5,8 +5,13 @@ namespace elevate\HVObjects\GenericTypes;
 
 use JMS\Serializer\Annotation\Type;
 use JMS\Serializer\Annotation\SerializedName;
+use JMS\Serializer\Annotation\XmlMap;
+use JMS\Serializer\Annotation\XmlRoot;
+use JMS\Serializer\Annotation\XmlAttribute;
 use JMS\Serializer\Annotation\XmlList;
-class CodableValue 
+use JMS\Serializer\Annotation\Groups;
+
+class CodableValue
 {
 
     /**
@@ -20,5 +25,11 @@ class CodableValue
      * @Type("array<game\XMLObjects\Types\Generic\CodedValue>")
      */
     protected $codes;
+
+    public function __construct($text,array $codes = array())
+    {
+        $this->text = $text;
+        $this->codes = $codes;
+    }
 
 }
