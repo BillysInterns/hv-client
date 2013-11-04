38d3b20ea19b2833effa6cdc749c6edfcdab47f9
Bryan N <bryan@webizly.com>
2013-11-04 13:22:15 -0500

    Unit Tests for Person thing type and everything contained within

---
+++ tests/elevate/HVObjects/Generic/PhoneTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: bnissen
+ * Date: 11/4/13
+ * Time: 9:49 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+namespace elevate\test\HVObjects\Generic;
+
+
+class PhoneTest {
+
+}
