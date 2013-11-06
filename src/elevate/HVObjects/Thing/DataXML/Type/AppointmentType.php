e78104a6e704ffab5768eb983c7eaf76f15285cf
palsumit <sumit@Sumits-MacBook-Pro.local>
2013-11-06 16:15:56 -0500

    AppointmentType and unit test

--- src/elevate/HVObjects/Thing/DataXML/Type/AppointmentType.php
+++ src/elevate/HVObjects/Thing/DataXML/Type/AppointmentType.php
@@ -1,17 +1,41 @@
 <?php
 
 namespace elevate\HVObjects\Thing\DataXML\Type;
+use JMS\Serializer\Annotation\Type;
+use JMS\Serializer\Annotation\SerializedName;
+use JMS\Serializer\Annotation\XmlMap;
+use JMS\Serializer\Annotation\XmlRoot;
+use JMS\Serializer\Annotation\XmlAttribute;
+use JMS\Serializer\Annotation\XmlList;
+use JMS\Serializer\Annotation\Groups;
+use PhpCollection\Map;
+use PhpCollection\Sequence;
 
-
+use elevate\HVObjects\Generic\CodedValue;
+use elevate\HVObjects\Generic\CodableValue;
+use elevate\HVObjects\Generic\Prescription;
+use elevate\HVObjects\Generic\GeneralMeasurement;
+use elevate\HVObjects\Generic\Date\ApproxDateTime;
+use elevate\HVObjects\Generic\Person;
+use elevate\HVObjects\Generic\Date\DurationValue;
+use elevate\HVObjects\Generic\Date\DateTime;
 
 /** @XmlRoot("appointment") */
 class AppointmentType
 {
+	/**
+	* @Type("elevate\HVObjects\Generic\Date\DateTime")
+	* @SerializedName("when")
+	*/
 	protected $when;
 
+	/**
+	* @Type("elevate\HVObjects\Generic\Date\DurationValue")
+	* @SerializedName("duration")
+	*/
 	protected $duration;
 
-	/*
+	/**
 	* @Type("elevate\HVObjects\Generic\CodableValue")
 	* @SerializedName("service")
 	*/
@@ -23,21 +47,33 @@ class AppointmentType
      */
 	protected $clinic;
 
-	/*
+	/**
 	* @Type("elevate\HVObjects\Generic\CodableValue")
 	* @SerializedName("specialty")
 	*/
 	protected $specialty;
 
-	/*
+	/**
 	* @Type("elevate\HVObjects\Generic\CodableValue")
 	* @SerializedName("status")
 	*/
 	protected $status;
 
-	/*
+	/**
 	* @Type("elevate\HVObjects\Generic\CodableValue")
 	* @SerializedName("care-class")
 	*/
-	protected $careClass
+	protected $careClass;
+
+    public function __construct($careClass, $clinic, $duration, $service, $specialty, $status, DateTime $when)
+    {
+        $this->careClass = $careClass;
+        $this->clinic = $clinic;
+        $this->duration = $duration;
+        $this->service = $service;
+        $this->specialty = $specialty;
+        $this->status = $status;
+        $this->when = $when;
+    }
+
 }
