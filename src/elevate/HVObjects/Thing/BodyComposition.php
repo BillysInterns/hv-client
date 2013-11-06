c307eec585e75fd7a3625a2182cdb303728b1719
Tyler Roussos <tylerroussos@gmail.com>
2013-11-06 13:57:49 -0500

    added body comp and unit tests

---
+++ src/elevate/HVObjects/Thing/BodyComposition.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * @author troussos
+ */
+
+namespace elevate\HVObjects\Thing;
+
+use JMS\Serializer\Annotation\XmlRoot;
+
+use JMS\Serializer\Annotation\Type;
+use JMS\Serializer\Annotation\SerializedName;
+use JMS\Serializer\Annotation\XmlList;
+
+use elevate\TypeTranslator;
+use elevate\HVObjects\Thing\Thing;
+
+use elevate\HVObjects\Thing\DataXML\BodyCompositionDataXML;
+
+/** @XmlRoot("thing") */
+class BodyComposition extends Thing
+{
+
+    /**
+     * @var array elevate\HVObjects\Thing\DataXML\BodyCompositionDataXML
+     * @Type("elevate\HVObjects\Thing\DataXML\BodyCompositionDataXML")
+     * @serializedName("data-xml")
+     */
+    protected $dataXML;
+
+    /**
+     * @param $dataXML
+     */
+    public function __construct(BodyCompositionDataXML $dataXML)
+    {
+        $typeID = TypeTranslator::lookupTypeID('Body Composition');
+        parent::__construct($dataXML,$typeID);
+    }
+
+
+    /**
+     * @return array
+     */
+    public function getDataXML()
+    {
+        return $this->dataXML;
+    }
+
+    /**
+     * @param array $dataXML
+     */
+    public function setDataXML($dataXML)
+    {
+        $this->dataXML = $dataXML;
+        return $this;
+    }
+
+
+}
