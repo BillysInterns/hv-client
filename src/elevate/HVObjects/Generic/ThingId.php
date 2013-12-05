f47c0281713bb9b0182eb1242da5de0e9b41cd8d
Tyler Roussos <tylerroussos@gmail.com>
2013-12-05 11:32:51 -0500

    Added ThingId and unit test

---
+++ src/elevate/HVObjects/Generic/ThingId.php
@@ -0,0 +1,78 @@
+<?php
+/**
+* @author - troussos
+*/
+
+namespace elevate\HVObjects\Generic;
+
+use JMS\Serializer\Annotation\Type;
+use JMS\Serializer\Annotation\SerializedName;
+use JMS\Serializer\Annotation\XmlMap;
+use JMS\Serializer\Annotation\XmlRoot;
+use JMS\Serializer\Annotation\XmlAttribute;
+use JMS\Serializer\Annotation\XmlList;
+use JMS\Serializer\Annotation\XmlValue;
+use JMS\Serializer\Annotation\Groups;
+use PhpCollection\Map;
+use PhpCollection\Sequence;
+
+
+class ThingId
+{
+    /**
+     * @XmlAttribute
+     * @Type("string")
+     */
+    protected $version;
+
+    /**
+     * @XmlValue
+     * @Type("string")
+     */
+    protected $thingId;
+
+    function __construct($thingId, $version)
+    {
+        $this->thingId = $thingId;
+        $this->version = $version;
+        return $this;
+    }
+
+    /**
+     * @param $thingId
+     *
+     * @return $this
+     */
+    public function setThingId($thingId)
+    {
+        $this->thingId = $thingId;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getThingId()
+    {
+        return $this->thingId;
+    }
+
+    /**
+     * @param $version
+     *
+     * @return $this
+     */
+    public function setVersion($version)
+    {
+        $this->version = $version;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getVersion()
+    {
+        return $this->version;
+    }
+}
