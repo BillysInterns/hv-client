5d66699944a268a502af39254d871a92281acaa8
ofields <ofields@gmail.com>
2013-12-18 14:55:20 -0500

    Reverted back the autoload.php

--- autoload.php
+++ autoload.php
@@ -1,9 +1,9 @@
 <?php
 
-    require_once('/var/www/mentis-web/vendor/autoload.php');
+    require_once('vendor/autoload.php');
 
 // Bootstrap the JMS custom annotations for Object to Json mapping
     \Doctrine\Common\Annotations\AnnotationRegistry::registerAutoloadNamespace(
         'JMS\Serializer\Annotation',
-        '/var/www/mentis-web/vendor/jms/serializer/src'
+        'vendor/jms/serializer/src'
     );
