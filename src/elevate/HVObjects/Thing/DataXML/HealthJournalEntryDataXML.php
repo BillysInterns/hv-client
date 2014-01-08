624f0d2a2a1287a1c6d1b817c15ab8d4b0a4c568
sumit <sumtipalmonga@gmail.com>
2014-01-08 15:08:42 -0500

    Health Journal DataXML

---
+++ src/elevate/HVObjects/Thing/DataXML/HealthJournalEntryDataXML.php
@@ -0,0 +1,52 @@
+<?php
+/**
+* @author - Sumit
+*/
+
+namespace elevate\HVObjects\Thing\DataXML;
+
+use JMS\Serializer\Annotation\Type;
+use JMS\Serializer\Annotation\SerializedName;
+use JMS\Serializer\Annotation\XmlMap;
+use JMS\Serializer\Annotation\XmlRoot;
+use JMS\Serializer\Annotation\XmlAttribute;
+use JMS\Serializer\Annotation\XmlList;
+use JMS\Serializer\Annotation\Groups;
+use JMS\Serializer\Annotation\AccessorOrder;
+use elevate\HVObjects\Thing\DataXML\DataXML;
+
+use elevate\HVObjects\Thing\DataXML\Type\HealthJournalEntryType;
+use elevate\HVObjects\Generic\Common;
+
+/** @XmlRoot("health-journal-entry") */
+class HealthJournalEntryDataXML extends DataXML
+{
+    /**
+     * @Type("elevate\HVObjects\Thing\DataXML\Type\HealthJournalEntryType")
+     * @SerializedName("health-journal-entry")
+     */
+    protected $healthJournalEntry;
+
+    /**
+     * @param HealthJournalEntryType $healthJournalEntry
+     */
+    public function setHealthJournalEntry(HealthJournalEntryType $healthJournalEntry)
+    {
+        $this->healthJournalEntry = $healthJournalEntry;
+    }
+
+    function __construct(HealthJournalEntryType $healthJournalEntry = NULL, Common $common = NULL)
+    {
+        $this->healthJournalEntry = $healthJournalEntry;
+        parent::__construct($common);
+    }
+
+    /**
+     * @return HealthJournalEntryType
+     */
+    public function getHealthJournalEntry()
+    {
+        return $this->healthJournalEntry;
+    }
+
+}
