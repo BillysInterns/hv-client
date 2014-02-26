8676161d61baff2cfb7dd189239f0fb1b5de5f1c
sumit <sumtipalmonga@gmail.com>
2014-02-26 14:31:03 -0500

    style changes

--- src/elevate/HVObjects/Thing/DataXML/QuestionAnswerDataXML.php
+++ src/elevate/HVObjects/Thing/DataXML/QuestionAnswerDataXML.php
@@ -1,54 +1,54 @@
 <?php
 
-/**
- * @author ofields
- */
+    /**
+     * @author ofields
+     */
 
-namespace elevate\HVObjects\Thing\DataXML;
+    namespace elevate\HVObjects\Thing\DataXML;
 
-use JMS\Serializer\Annotation\Type;
-use JMS\Serializer\Annotation\SerializedName;
-use JMS\Serializer\Annotation\XmlMap;
-use JMS\Serializer\Annotation\XmlRoot;
-use JMS\Serializer\Annotation\XmlAttribute;
-use JMS\Serializer\Annotation\XmlList;
-use JMS\Serializer\Annotation\Groups;
-use JMS\Serializer\Annotation\AccessorOrder;
+    use JMS\Serializer\Annotation\Type;
+    use JMS\Serializer\Annotation\SerializedName;
+    use JMS\Serializer\Annotation\XmlMap;
+    use JMS\Serializer\Annotation\XmlRoot;
+    use JMS\Serializer\Annotation\XmlAttribute;
+    use JMS\Serializer\Annotation\XmlList;
+    use JMS\Serializer\Annotation\Groups;
+    use JMS\Serializer\Annotation\AccessorOrder;
 
-use elevate\HVObjects\Generic\Common;
-use elevate\HVObjects\Thing\DataXML\Type\QuestionAnswerType;
-
-/**
- * Class QuestionAnswer
- * @package elevate\HVObjects\Thing\DataXML
- * @AccessorOrder("custom", custom = {"questionType", "common"})
- */
-class QuestionAnswerDataXML extends DataXML
-{
+    use elevate\HVObjects\Generic\Common;
+    use elevate\HVObjects\Thing\DataXML\Type\QuestionAnswerType;
 
     /**
-     * @Type("elevate\HVObjects\Thing\DataXML\Type\QuestionAnswerType")
-     * @SerializedName("question-answer")
+     * Class QuestionAnswer
+     * @package elevate\HVObjects\Thing\DataXML
+     * @AccessorOrder("custom", custom = {"questionType", "common"})
      */
-    protected $questionType;
-
-    function __construct(QuestionAnswerType $questionType = NULL, Common $common = NULL)
+    class QuestionAnswerDataXML extends DataXML
     {
-        $this->questionType = $questionType;
-        $this->common       = $common;
-    }
 
-    public function setType(QuestionAnswerType $questionType)
-    {
-        $this->questionType = $questionType;
-        return $this;
-    }
+        /**
+         * @Type("elevate\HVObjects\Thing\DataXML\Type\QuestionAnswerType")
+         * @SerializedName("question-answer")
+         */
+        protected $questionType;
 
-    /**
-     * @return \elevate\HVObjects\Thing\DataXML\Type\QuestionAnswerType
-     */
-    public function getType()
-    {
-        return $this->questionType;
+        function __construct(QuestionAnswerType $questionType = NULL, Common $common = NULL)
+        {
+            $this->questionType = $questionType;
+            $this->common       = $common;
+        }
+
+        public function setType(QuestionAnswerType $questionType)
+        {
+            $this->questionType = $questionType;
+            return $this;
+        }
+
+        /**
+         * @return \elevate\HVObjects\Thing\DataXML\Type\QuestionAnswerType
+         */
+        public function getType()
+        {
+            return $this->questionType;
+        }
     }
-}
