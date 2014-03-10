d4ffebb733a8cbdcd91d9a930bff746587141852
ecompton <ecomton3@gmail.com>
2014-03-10 18:09:15 -0400

    added keys, added path to keys, changed record id to one with full access

--- tests/elevate/BaseTest.php
+++ tests/elevate/BaseTest.php
@@ -47,26 +47,26 @@ class BaseTest extends \PHPUnit_Framework_TestCase
      */
     protected function setUp()
     {
-        $baseConfigPath = realpath("");
-        if (file_exists($baseConfigPath . '/sample_keys/app.id'))
+         $path = __DIR__.'/sample_keys/app.id';
+        if (file_exists(__DIR__.'/sample_keys/app.id'))
         {
-            $this->appId = file_get_contents($baseConfigPath . '/sample_keys/app.id');
+            $this->appId = file_get_contents(__DIR__.'/sample_keys/app.id');
         }
 
-        if (file_exists($baseConfigPath . '/sample_keys/app.fp'))
+        if (file_exists(__DIR__.'/sample_keys/app.fp'))
         {
-            $this->thumbPrint = file_get_contents($baseConfigPath . '/sample_keys/app.fp');
+            $this->thumbPrint = file_get_contents(__DIR__.'/sample_keys/app.fp');
         }
 
-        if (file_exists($baseConfigPath . '/sample_keys/server.key'))
+        if (file_exists(__DIR__.'/sample_keys/server.key'))
         {
-            $this->privateKey = file_get_contents($baseConfigPath . '/sample_keys/server.key');
+            $this->privateKey = file_get_contents(__DIR__.'/sample_keys/server.key');
         }
 
         $this->session    = array();
 
         $this->personId   = 'fa295fdb-c334-4c8f-b332-ec62074df84e';
-        $this->recordId   = '8813bcc7-4075-4f96-a8ea-8df6d184f3fa';
+        $this->recordId   = '40085a94-3ddb-4eb4-b501-ff1ca5e4e2ba';
         if (!is_null($this->privateKey) && !is_null($this->appId) && !is_null($this->privateKey))
         {
             $this->hv = new HVClient($this->thumbPrint, $this->privateKey, $this->appId, $this->personId, $this->recordId);
